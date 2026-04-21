e238123540f83874caa3baba009aef851fbece16
Jesus Obispo <jesus.obispo@educa.madrid.org>
2026-04-21 15:31:08 +0200

    tokenkey

--- src/services/token.service.js
+++ src/services/token.service.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken'
 export function createToken(userInfo) {
     console.log("este es el servicio de token: ",userInfo)
-    const token=jwt.sign(userInfo, '123123456')
+    const token=jwt.sign(userInfo, process.env.tokenKey)
     return token
 }
