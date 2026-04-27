4d87a2327a8f5dfd9c8f5661a9d07f270de0d383
Jesus Obispo <jesus.obispo@educa.madrid.org>
2026-04-27 15:36:18 +0200

    cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import userRoutes from './src/routes/user.route.js';
-
+import cors from 'cors'
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cors());
+
 
 app.get('/', (req, res) => {
   res.json({ message: 'API de pruebas funcionando' });
